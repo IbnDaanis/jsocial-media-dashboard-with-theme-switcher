e949f8101db21ec1e43de3d13ba0c33207f313b9
Ikraam <IbnDaanis@Gmail.com>
2020-10-21 14:13:40 +0100

    Make dark mode toggle feature with local storage

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
 const myToggle = document.querySelector('#myToggle')
-const toggle = document.querySelector('.toggle')
+const toggleButton = document.querySelector('.toggle')
 const count = document.querySelectorAll('.count')
 const card = document.querySelectorAll('.card')
 const number = document.querySelectorAll('.number')
 const headingTitle = document.querySelector('.nav-title h1')
 const sectionTitle = document.querySelector('.section-title h2')
 
-myToggle.addEventListener('click', e => {
+myToggle.checked = JSON.parse(localStorage.getItem('checked'))
 
-  if (myToggle.checked === true) {
-    console.log('checked')
-    document.body.classList.toggle('body-dark')
-    headingTitle.classList.toggle('dark-font-white')
-    sectionTitle.classList.toggle('dark-font-white')
-    count.forEach(x => x.classList.toggle('dark-font-white'))
-    card.forEach(x => x.classList.toggle('card-dark'))
-    number.forEach(x => x.classList.toggle('dark-font-white'))
-    toggle.classList.toggle('toggle-dark')
+function setDarkMode() {
+  document.body.classList.toggle('body-dark')
+  headingTitle.classList.toggle('dark-font-white')
+  sectionTitle.classList.toggle('dark-font-white')
+  count.forEach(item => item.classList.toggle('dark-font-white'))
+  card.forEach(item => item.classList.toggle('card-dark'))
+  number.forEach(item => item.classList.toggle('dark-font-white'))
+  toggleButton.classList.toggle('toggle-dark')
+  localStorage.setItem('checked', myToggle.checked)
+}
 
-  } else if (myToggle.checked === false) {
-    document.body.classList.toggle('body-dark')
-    headingTitle.classList.toggle('dark-font-white')
-    sectionTitle.classList.toggle('dark-font-white')
-    count.forEach(x => x.classList.toggle('dark-font-white'))
-    card.forEach(x => x.classList.toggle('card-dark'))
-    number.forEach(x => x.classList.toggle('dark-font-white'))
-    console.log('unchecked')
-    toggle.classList.toggle('toggle-dark')
-  }
-})
+!myToggle.checked && setDarkMode()
+
